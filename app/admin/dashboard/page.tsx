2e9555cadd0e500915e14e68587248ba8283ee74
Vamshi CH <chvamshi03@gmail.com>
2025-05-22 16:09:00 +0530

    updated admin

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "../../api/auth/[...nextauth]/route"
+import { authOptions } from "@/lib/auth-options"
 import prisma from "@/lib/prisma"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Mail, Award, Dumbbell } from "lucide-react"
